e0c130232cdb538df81e519018ce63d6307c30e9
sunvirgo <g_sun2java@163.com>
2020-05-08 00:28:58 +0800

    sunvirgo_hg:day15_登录&拦截器

--- src/main/java/com/example/springbootstep04/config/MyMvconfig.java
+++ src/main/java/com/example/springbootstep04/config/MyMvconfig.java
@@ -1,14 +1,17 @@
 package com.example.springbootstep04.config;
 
+import com.example.springbootstep04.component.LoginHandleInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * 类说明:
- *使用WebMvcConfigurerAdapter可以扩展SpringMvc功能
+ * 使用WebMvcConfigurerAdapter可以扩展SpringMvc功能
+ *
  * @Author: 黄刚
  * @Date: 2020/5/5 22:03
  */
@@ -26,13 +29,27 @@ public class MyMvconfig extends WebMvcConfigurerAdapter {
 
     //所有的WebMvcConfigurerAdapter组件会一起起作用
     @Bean//将组件祖册在容器中
-    public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
-        WebMvcConfigurerAdapter webMvcConfigurerAdapter = new WebMvcConfigurerAdapter(){
+    public WebMvcConfigurerAdapter webMvcConfigurerAdapter() {
+        WebMvcConfigurerAdapter webMvcConfigurerAdapter = new WebMvcConfigurerAdapter() {
             @Override
             public void addViewControllers(ViewControllerRegistry registry) {
                 registry.addViewController("/").setViewName("login");
                 registry.addViewController("/index.html").setViewName("login");
+                registry.addViewController("/main.html").setViewName("dashboard");
             }
+
+            //注册拦截器
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                //静态资源：*.css,*.js
+                //springboot已经做好了静态资源映射,然而世纪被拦截了
+                //这样对于利用HandlerInterceptor来处理访问权限或其他相关的功能就会受影响，跟踪源码查看原因，是因为spring boot 2.x依赖的spring 5.x版本，
+                // 相对于spring boot 1.5.x依赖的spring 4.3.x版本而言，针对资源的拦截器初始化时有区别,
+                //从源码当中可以看出，使用spring 5.x时，静态资源也会执行自定义的拦截器，因此在配置拦截器的时候需要指定排除静态资源的访问路径.
+                registry.addInterceptor(new LoginHandleInterceptor()).addPathPatterns("/**").excludePathPatterns("/index.html", "/", "/user/login", "/docs/**", "/webjars/**");
+                //super.addInterceptors(registry);
+            }
+
         };
 
         return webMvcConfigurerAdapter;
---
+++ src/main/java/com/example/springbootstep04/controller/LoginController.java
@@ -0,0 +1,36 @@
+package com.example.springbootstep04.controller;
+
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+/**
+ * 类说明:
+ *
+ * @Author: 黄刚
+ * @Date: 2020/5/7 22:16
+ */
+@Controller
+public class LoginController {
+    @PostMapping(value = "/user/login")
+    //@RequestMapping(value = "/user/login",method = RequestMethod.POST)
+    public String login(@RequestParam("username")String username, @RequestParam("password")String password, Map<String,Object> map, HttpSession session){
+        if(!StringUtils.isEmpty(username) && "123456".equals(password)){
+            //登陆成功,防止表单重复提交，可以重定向到主页
+            session.setAttribute("loginUser",username);
+            return "redirect:/main.html";
+        }else{
+            //登录失败
+            map.put("msg","用户名或密码错误！");
+            return "login";
+        }
+
+    }
+}
