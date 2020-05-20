2370bbbb563b69c2df8c03a978a4c26f7ceb825f
sunvirgo <g_sun2java@163.com>
2020-05-20 23:39:06 +0800

    sunvirgo_hg:day26_切换其他嵌入式Servlet容器

--- src/main/java/com/example/springbootstep04/config/MyMvconfig.java
+++ src/main/java/com/example/springbootstep04/config/MyMvconfig.java
@@ -2,14 +2,12 @@ package com.example.springbootstep04.config;
 
 import com.example.springbootstep04.component.LoginHandleInterceptor;
 import org.apache.catalina.connector.Connector;
-import org.springframework.boot.autoconfigure.web.embedded.EmbeddedWebServerFactoryCustomizerAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
