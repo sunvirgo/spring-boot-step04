e28eaef853431e10120e64bf4ad9c989cfa9fd4d
sunvirgo <g_sun2java@163.com>
2020-06-08 23:58:05 +0800

    sunvirgo_hg:day36_整合Druid数据源

---
+++ day35-data-jdbc/src/main/java/com/sunvirgo/day35datajdbc/config/DruidConfig.java
@@ -0,0 +1,67 @@
+package com.sunvirgo.day35datajdbc.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 类说明:
+ *
+ * @author : 黄刚
+ * @date : 2020/6/8 23:26
+ **/
+@Configuration
+public class DruidConfig {
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DataSource druid(){
+        return new DruidDataSource();
+    }
+    /**
+     * 方法说明: 配置Durid的监控:1.配置一个管理后台的Servlet
+     * @author : 黄刚
+     * @date : 2020/6/8 23:30
+     * @para : []
+     * @return : org.springframework.boot.web.servlet.ServletRegistrationBean
+     */
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        Map<String,String> initParams = new HashMap<>();
+        initParams.put("loginUsername","admin");
+        initParams.put("loginPassword","123456");
+        initParams.put("allow","");//默认允许所有访问
+        initParams.put("deny","localhost");
+        bean.setInitParameters(initParams);
+        return bean;
+    }
+
+    /**
+     * 方法说明: 2.配置一个WEB监控的Filter
+     * @author : 黄刚
+     * @date : 2020/6/8 23:41
+     * @para :
+     * @return :
+     */
+    @Bean
+    public FilterRegistrationBean webStatFilter(){
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setFilter(new WebStatFilter());
+        Map<String,String> initParams = new HashMap<>();
+        initParams.put("exclusions","*.js,*.css,/druid/*");
+        bean.setInitParameters(initParams);
+        bean.setUrlPatterns(Arrays.asList("/*"));
+        return bean;
+    }
+}
